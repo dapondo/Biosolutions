66fdade6c2816548169c7ee8c280a09a6256c08a
dapondo <dannygomezpineda@hotmail.com>
2017-04-21 21:38:54 -0500

    Update indexclientepotencialView.php

--- view/clientepotencial/indexclientepotencialView.php
+++ view/clientepotencial/indexclientepotencialView.php
@@ -1,29 +1,28 @@
-<!--CrearCliente-->        
+<!--CrearCliente-->   
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <br>
+        <div class="container well ">
 
-        <form id="formregistrar" method="POST" action="<?php echo "index.php?controller=clientepotencial&action=crear";?>">
-            <h1>Crear cliente</h1>
-            <br>
-            <input id="input-nit-consultar" type="text" name="cp_nit" placeholder="Nit">
-            <br>
-            <br>
-            <input id="input-nombre-consultar" type="text" name="cp_nombre" placeholder="Nombre">
-            <br>
-            <br>
-            <input id="input-ciudad-consultar" type="text" name="cp_ciudad" placeholder="Ciudad">
-            <br>
-            <br>
-            <input id="input-direccion-consultar" type="text" name="cp_direccion" placeholder="Dirección">
-            <br>
-            <br>
-            <input id="input-telefono-consultar" type="text" name="cp_telefono" placeholder="Telefono">
-            <br>
-            <br>
-            <input  id="input-observaciones-consultar" type="textarea" name="cp_observaciones" placeholder="Observaciones">
-            <br>
-            <br>
-            <input id="input-consultar" type="submit" name="enviar"  value="Crear">
-            <br>
-            <br>
+
+            <form   class="row well" method="POST" action="<?php echo "index.php?controller=clientepotencial&action=crear"; ?>">
+                <h1>Crear cliente</h1>
+
+                <div class="col-xs-2"> <input class="form-control" id="input-nit-consultar" type="text" name="cp_nit" placeholder="Nit"></div>
+                <div class="col-xs-2"><input class="form-control" id="input-nombre-consultar" type="text" name="cp_nombre" placeholder="Nombre"></div>
+                <div class="col-xs-2"><input class="form-control" id="input-ciudad-consultar" type="text" name="cp_ciudad" placeholder="Ciudad"></div>
+                <div class="col-xs-2"><input class="form-control" id="input-direccion-consultar" type="text" name="cp_direccion" placeholder="Dirección"></div>
+                <div class="col-xs-2"><input class="form-control" id="input-telefono-consultar" type="text" name="cp_telefono" placeholder="Telefono"></div>
+                <div class="col-xs-2"><input class="form-control" id="input-observaciones-consultar" type="textarea" name="cp_observaciones" placeholder="Observaciones"></div>
+                <div class="col-xs-2"><br><input class="form-control btn-success" id="input-consultar" type="submit" name="enviar"  value="Crear"></div>
             </form>
             <section class="clientepotencial" id="client">
-                
+        </div>
+        
+
+
+    </body>
+</html>
