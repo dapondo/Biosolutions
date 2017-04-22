29b2cd9eaa5d59f690ecf07a050a9306f06aad0e
dapondo <dannygomezpineda@hotmail.com>
2017-04-21 23:22:48 -0500

    Update indexclientepotencialView.php

--- view/clientepotencial/indexclientepotencialView.php
+++ view/clientepotencial/indexclientepotencialView.php
@@ -8,15 +8,15 @@
         <div class="container well ">
 
 
-            <form   class="row well" method="POST" action="<?php echo "index.php?controller=clientepotencial&action=crear"; ?>">
-                <h1>Crear cliente</h1>
+            <form   class="row" method="POST" action="<?php echo "index.php?controller=clientepotencial&action=crear"; ?>">
+                <div class=" well "> <h1>Crear cliente potencial</h1>  </div>
 
                 <div class="col-xs-2"> <input class="form-control" id="input-nit-consultar" type="text" name="cp_nit" placeholder="Nit"></div>
                 <div class="col-xs-2"><input class="form-control" id="input-nombre-consultar" type="text" name="cp_nombre" placeholder="Nombre"></div>
                 <div class="col-xs-2"><input class="form-control" id="input-ciudad-consultar" type="text" name="cp_ciudad" placeholder="Ciudad"></div>
                 <div class="col-xs-2"><input class="form-control" id="input-direccion-consultar" type="text" name="cp_direccion" placeholder="Dirección"></div>
                 <div class="col-xs-2"><input class="form-control" id="input-telefono-consultar" type="text" name="cp_telefono" placeholder="Telefono"></div>
-                <div class="col-xs-2"><input class="form-control" id="input-observaciones-consultar" type="textarea" name="cp_observaciones" placeholder="Observaciones"></div>
+                <div class="col-xs-5"><br><textarea class="form-control" id="input-observaciones-consultar" cols="50" rows="10" type="textarea" name="cp_observaciones" placeholder="Observaciones"></textarea></div>
                 <div class="col-xs-2"><br><input class="form-control btn-success" id="input-consultar" type="submit" name="enviar"  value="Crear"></div>
             </form>
             <section class="clientepotencial" id="client">
