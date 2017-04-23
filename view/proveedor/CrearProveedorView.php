cc257d57328c5d4e1e07d519601e00201debe89b
dapondo <dannygomezpineda@hotmail.com>
2017-04-23 10:47:36 -0500

    Update CrearProveedorView.php

--- view/proveedor/CrearProveedorView.php
+++ view/proveedor/CrearProveedorView.php
@@ -6,28 +6,29 @@
     </head>
     <body>
 
-        <div>
-            <form id="CrearProveedor" action="index.php?controller=Proveedor&action=crearproveedor" method="POST">
-                <p>Nit</p>
-                <input type="text" name="pro_nit">
-                <p>Nombre</p>
-                <input type="text" name="pro_Nombre">
-                <p>Email</p>
-                <input type="text" name="pro_emailEmpresa">
-                <p>Pagina Web</p>
-                <input type="text" name="pro_paginaWeb">
-                <p>Dirección</p>
-                <input type="text" name="pro_direccion">
-                <p>Telefono</p>
-                <input type="text" name="pro_telefono">
-                <p>Pais</p>
-                <input type="text" name="pro_pais">
-                <p>Ciudad</p>
-                <input type="text" name="pro_ciudad">
-                <button id="btncrear" type="submit">Crear</button>
+        <div class="container well col-xs-12 col-md-4  col-md-offset-4">
+            <div class="well col-xs-12 col-lg-12"><h3 style="text-align: center">Nuevo proveedor</h3></div>
+            <form class="well col-xs-12 col-lg-12" action="index.php?controller=Proveedor&action=crearproveedor" method="POST">
+                <div>Nit</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_nit"></div>
+                <div>Nombre</div>
+                <div class="form-group"><input class="form-control" type="text"  name="pro_Nombre"></div>
+                <div>Email</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_emailEmpresa"></div>
+                <div>Pagina Web</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_paginaWeb"></div>
+                <div>Dirección</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_direccion"></div>
+                <div>Telefono</div>
+                <div class="form-group"><input  class="form-control" type="text" name="pro_telefono"></div>
+                <div>Pais</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_pais"></div>
+                <div>Ciudad</div>
+                <div class="form-group"><input class="form-control" type="text" name="pro_ciudad"></div>
+                <div><br><button class="btn-success form-control" type="submit">Crear</button></div>
             </form>
-            
+
         </div>
-        
+
     </body>
 </html>
