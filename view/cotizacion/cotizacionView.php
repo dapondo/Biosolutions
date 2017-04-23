7d49032d73b4ab8ab707a11bd3ce87554df2bf21
dapondo <dannygomezpineda@hotmail.com>
2017-04-23 09:19:19 -0500

    Update cotizacionView.php

--- view/cotizacion/cotizacionView.php
+++ view/cotizacion/cotizacionView.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Tabla cotizacion</title>
+        <title>Cotizacion</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body>
-        <div>
-            <form id="crearproducto" method="POST" action="<?php echo "index.php?controller=Cotizacion&action=index"; ?>">
-                <p>Codigo de Cotizacion</p>
-                <input type="text" id="cot_codigoCotizacion"  name="cot_codigoCotizacion">
-                <p>Cliente</p>
-                <input type="text" id="con_codigo"  name="con_codigo">
-                <p>validez</p>
-                <input type="text" id="cot_validez"  name="cot_validez">
-                <p>Tiempo de Entrega</p>
-                <input type="text" id="cot_tiempoEntrega"  name="cot_tiempoEntrega">
-                <p>Lugar de entrega</p>
-                <input type="text" id="cot_lugarEntrega"  name="cot_lugarEntrega">
-                <p>Forma de pago</p>
-                <input type="text" id="cot_fechaCotizacion"  name="cot_formaPago">
-                <p>Fecha de vencimiento de cotizacion</p>
-                <input type="date" id="prd_fechaVencimiento"  name="cot_fechaCotizacion">
+    <body><br>
+        <div class="container well col-xs-12 col-md-4  col-md-offset-4">
+            <div class="well col-xs-12 col-md-12"><h3>Crear cotización</h3></div>
+            <form  class="well col-xs-12 col-md-12"   method="POST" action="<?php echo "index.php?controller=Cotizacion&action=index"; ?>">
+                <div class="form-group"><p>Codigo de Cotizacion</p></div>
+                <div><input type="text" class="form-control" id="cot_codigoCotizacion"  name="cot_codigoCotizacion"></div>
+                <div class="form-group"><p>Cliente</p></div>
+                <div><input type="text" class="form-control" id="con_codigo"  name="con_codigo"></div>
+                <div class="form-group"><p>validez</p></div>
+                <div><input type="text" class="form-control" id="cot_validez"  name="cot_validez"></div>
+                <div class="form-group"><p>Tiempo de Entrega</p></div>
+                <div><input type="text" class="form-control" id="cot_tiempoEntrega"  name="cot_tiempoEntrega"></div>
+                <div class="form-group"><p>Lugar de entrega</p></div>
+                <div><input type="text" class="form-control" id="cot_lugarEntrega"  name="cot_lugarEntrega"></div>
+                <div class="form-group"><p>Forma de pago</p></div>
+                <div><input type="text" class="form-control" id="cot_fechaCotizacion"  name="cot_formaPago"></div>
+                <div class="form-group"><p>Fecha de vencimiento de cotizacion</p></div>
+                <div><input type="date" class="form-control" id="prd_fechaVencimiento"  name="cot_fechaCotizacion"></div>
                 <br>
                 <br>
-                <input type="submit">
+                <input class="btn btn-success form-control"type="submit">
             </form>
         </div>
     </body>
 </html>
+
